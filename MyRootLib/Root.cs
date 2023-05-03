1b4809d9cf2500cea459f7e94125fa13710a40be
efiemaron <123518567+efiemaron@users.noreply.github.com>
2023-05-03 18:21:05 -0400

    Add files via upload

---
+++ MyRootLib/Root.cs
@@ -0,0 +1,27 @@
+﻿namespace Root
+{
+  public static class MoreMath
+{
+    
+    public static int Max(int x, int y)
+    {
+        return Math.Max(x, y);
+    }
+
+    public static int Max(int x, int y, int z)
+    {
+       
+        return Math.Max(x, Math.Max(y, z));
+    }
+
+    public static int Max(int w, int x, int y, int z)
+    {
+        return Math.Max(w, Math.Max(x, Math.Max(y, z)));
+    }
+
+    public static int Max(params int[] values)
+    {
+        return Enumerable.Max(values);
+    }
+}
+}
